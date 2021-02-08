85b015d244e9b4282c1a5248725a26b4d7df01c6
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2021-02-09 00:00:05 +0330

    edit class AI

--- src/client/AI.java
+++ src/client/AI.java
@@ -1,4 +1,21 @@
 package client;
 
+
+/**
+ * You must put your code in this class.
+ * turn, to do orders while game is running;
+ * end, to process after the end of the game;
+ */
+
 public class AI {
+
+    public void turn(/*TODO*/) {
+        System.out.println("turn started: ");
+        //some codes here
+    }
+
+    public void end(/*TODO*/) {
+        System.out.println("end started");
+        //some codes here
+    }
 }
