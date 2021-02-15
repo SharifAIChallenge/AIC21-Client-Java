66e3370fc4f014086ab50244b690b0a0ae4e6f67
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2021-02-15 19:02:33 +0330

    add method titles to the World

--- src/client/World.java
+++ src/client/World.java
@@ -1,4 +1,38 @@
 package client;
 
+import client.model.Ant;
+import client.model.ChatBox;
+import client.model.enums.AntType;
+import client.model.enums.Direction;
+
 public interface World {
+    public void sendDirection(Direction direction);
+
+    public void sendMessage(String message, int value);
+
+    public Ant getAnt();
+
+    public AntType getAntType();
+
+    public ChatBox getChatBox();
+
+    public int getMapWidth();
+
+    public int getMapHeight();
+
+    public int getBaseX();
+
+    public int getBaseY();
+
+    public int getHealthKargar();
+
+    public int getHealthSarbaaz();
+
+    public int getAttackDistance();
+
+    public int getGenerateKargar();
+
+    public int getGenerateSarbaaz();
+
+    public int getRateDeathResource();
 }
