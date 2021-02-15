575678405a1e34ecaede352167a39f5767246d14
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2021-02-15 19:32:57 +0330

    add some documentation to World

--- src/client/World.java
+++ src/client/World.java
@@ -10,6 +10,7 @@ public interface World {
 
     /**
      * use this method for choosing your moving direction
+     *
      * @param direction is an enum object (UP, DOWN, LEFT, RIGHT, CENTER)
      * you can access different direction using  {@link client.model.enums.Direction}
      */
@@ -19,15 +20,28 @@ public interface World {
      * use this method for sending you message and its value
      * you can only send one message in each turn
      * your message length must be smaller than or equal to {@link #MAX_MESSAGE_LENGTH}
+     *
      * @param message is your message text
-     * @param value is your message value
+     * @param value   is your message value
      */
     public void sendMessage(String message, int value);
 
+    /**
+     * @return your ant object  {@link client.model.Ant}
+     */
     public Ant getAnt();
 
+    /**
+     * @return your ant type
+     * AntType is an enum {@link client.model.enums.AntType}
+     */
     public AntType getAntType();
 
+    /**
+     * @return your chat box
+     * AntType is an enum {@link client.model.ChatBox}
+     * you can access every chat message {@link client.model.Chat} using ChatBox
+     */
     public ChatBox getChatBox();
 
     public int getMapWidth();
