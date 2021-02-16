cec689d40c828d9fa2dfdfd308431932caaac22b
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2021-02-17 01:03:05 +0330

    add comment to Direction

--- src/client/model/enums/Direction.java
+++ src/client/model/enums/Direction.java
@@ -1,5 +1,9 @@
 package client.model.enums;
 
+/**
+ * different types of direction for agent's next movement
+ * default value for direction is CENTER (in case of null objects or maybe other problems)
+ */
 public enum Direction {
     CENTER,
     RIGHT,
