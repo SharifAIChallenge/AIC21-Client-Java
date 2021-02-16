265bd31eaf5a3c741e7786e9c33ca87aaaffe7c1
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2021-02-16 21:50:32 +0330

    remove some useless comments

--- src/client/World.java
+++ src/client/World.java
@@ -3,28 +3,9 @@ package client;
 import client.model.Ant;
 import client.model.ChatBox;
 import client.model.enums.AntType;
-import client.model.enums.Direction;
 
 public interface World {
     int MAX_MESSAGE_LENGTH = 1000;
-
-//    /**
-//     * use this method for choosing your moving direction
-//     *
-//     * @param direction is an enum object (UP, DOWN, LEFT, RIGHT, CENTER)
-//     * you can access different direction using  {@link client.model.enums.Direction}
-//     */
-//    public void chooseDirection(Direction direction);
-//
-//    /**
-//     * use this method for sending you message and its value
-//     * you can only send one message in each turn
-//     * your message length must be smaller than or equal to {@link #MAX_MESSAGE_LENGTH}
-//     *
-//     * @param message is your message text
-//     * @param value   is your message value
-//     */
-//    public void sendMessage(String message, int value);
 
     /**
      * @return your ant object  {@link client.model.Ant}
