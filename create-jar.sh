c90195a7ada4d40e47257e016113e4e11145ae9d
m.dehghankar <mohsendehghankar@gmail.com>
2021-04-28 11:05:56 +0430

    remove ignored

--- create-jar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-find -name "*.java" > sources.txt
-javac -cp ./src @sources.txt -d ./out/ -classpath /home/mohsen/.m2/repository/com/google/code/gson/gson/2.8.6/gson-2.8.6.jar
-jar cvfm client.jar ./MANIFEST.MF -C ./out/ .
